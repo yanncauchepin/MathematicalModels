942b25f4b9781292e0e2292bd6ed374d924c736e
yanncauchepin <cauchepin.yann@gmail.com>
2024-06-15 17:54:59 +0200

    edit svd

--- singular_value_decomposition/singular_value_decomposition.py
+++ singular_value_decomposition/singular_value_decomposition.py
@@ -2,33 +2,58 @@ import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
 
-image_path = './data/thoune_5.jpg'
-image = Image.open(image_path)
-image_gray = image.convert('L')
-image_array = np.array(image_gray)
+def read_image(image_path):
+    image = Image.open(image_path)
+    image_gray = image.convert('L')
+    image_array = np.array(image_gray)
+    return image_array
 
-U, S, V = np.linalg.svd(image_array)
+def compute_svd(image_array):
+    U, S, V = np.linalg.svd(image_array)
+    return U, S, V
 
-num_eigenvalues = 100
+def define_truncated_matrices(U, S, V, num_eigenvalues):
+    U_truncated = U[:, :num_eigenvalues]
+    S_truncated = np.diag(S[:num_eigenvalues])
+    V_truncated = V[:num_eigenvalues, :]
+    return U_truncated, S_truncated, V_truncated
 
-# Truncate the singular value matrices
-U_truncated = U[:, :num_eigenvalues]
-S_truncated = np.diag(S[:num_eigenvalues])
-V_truncated = V[:num_eigenvalues, :]
+def plot_eigenvalues_evolution(S):
+    plt.figure(figsize=(6, 4))
+    plt.plot(np.arange(len(S)), S, 'o-')
+    plt.title('Eigenvalues')
+    plt.xlabel('Index')
+    plt.ylabel('Magnitude')
+    plt.show()
 
-# Reconstruct the image using the truncated matrices
-image_reconstructed = U_truncated @ S_truncated @ V_truncated
+def reconstruct_image(U_truncated, S_truncated, V_truncated):
+    image_reconstructed = U_truncated @ S_truncated @ V_truncated
+    return image_reconstructed
 
-# Display the original and reconstructed images
-fig, axs = plt.subplots(1, 2, figsize=(10, 5))
-axs[0].imshow(image_array, cmap='gray')
-axs[0].set_title('Original Image')
-axs[0].axis('off')
-axs[1].imshow(image_reconstructed, cmap='gray')
-axs[1].set_title(f'Reconstructed Image ({num_eigenvalues} eigenvalues)')
-axs[1].axis('off')
+def display_comparison_images(image_array, image_reconstructed):
+    fig, axs = plt.subplots(1, 2, figsize=(10, 5))
+    
+    original_size_kb = image_array.nbytes / 1024
+    reconstructed_size_kb = image_reconstructed.nbytes / 1024
+    
+    axs[0].imshow(image_array, cmap='gray')
+    axs[0].set_title(f'Original Image (size: {original_size_kb:.2f} kB)')
+    axs[0].axis('off')
+    
+    axs[1].imshow(image_reconstructed, cmap='gray')
+    axs[1].set_title(f'Reconstructed Image (size: {reconstructed_size_kb:.2f} kB, {num_eigenvalues} eigenvalues)')
+    axs[1].axis('off')
+    
+    plt.show()
 
-# Show the plot
-plt.show()
 
+if __name__=='__main__':
+    image_path = './dataset/tiger_forest.jpg'
+    image_array = read_image(image_path)
+    U, S, V = compute_svd(image_array)
+    plot_eigenvalues_evolution(S)
+    num_eigenvalues = 50
+    U_truncated, S_truncated, V_truncated = define_truncated_matrices(U, S, V, num_eigenvalues)
+    image_reconstructed = reconstruct_image(U_truncated, S_truncated, V_truncated)
+    display_comparison_images(image_array, image_reconstructed)
 
